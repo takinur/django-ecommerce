b8de3decdf3f29d79c0899e65d56034b7bbfedf8
takinur <takinurm@gmail.com>
2022-03-22 16:07:52 +0600

    Added Data Table

--- demoProject/urls.py
+++ demoProject/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from empApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index, name='home'),
     path('list/', views.list, name='list'),
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
